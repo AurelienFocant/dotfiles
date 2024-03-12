00a9f9089511534c67a435ff054e28a49a828264
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-03-12 20:36:50 +0100

    started script -- brew.sh -- to automatically install software through homebrew

---
+++ brew.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+#-----------------
+# Check if cask is installed through homebrew already
+# If not, tries to install it
+#-----------------
+
+casks="./cask_somebrew"
+sort $casks -o $casks
+
+while read cask; do
+	if brew list --cask | grep -q "^$cask\$"; then
+		echo $cask is already installed;
+	else
+		brew install --cask $cask;
+	fi
+done < $casks
+
+
+#-----------------
+# Does the same for formulae
+#-----------------
