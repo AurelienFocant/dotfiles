28db3cf48ae6bc03fd6bbfa612901c7f5a45f79b
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-03-13 14:24:49 +0100

    started work on script for macOS settings

---
+++ macOS.sh
@@ -0,0 +1,11 @@
+#------------------------------------------------------
+# To be checked before uncommenting :
+# 	defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+# 	from https://apple.stackexchange.com/questions/284467/how-to-set-finder-to-always-use-list-view
+# Supposed to make default list view
+#------------------------------------------------------
+
+
+#------------------------------------------------------
+# Also should find a script to make Download folder the one on iCloud
+#------------------------------------------------------
