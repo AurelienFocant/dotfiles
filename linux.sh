5bd424d7eae11a17a4eeca39ca8b95cc02bd21a8
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-07-10 23:37:47 +0200

    started script for quick install of a new lniux machine

---
+++ linux.sh
@@ -0,0 +1,11 @@
+sudo apt install sudo
+#usermod -aG sudo aurel
+sudo apt install ufw
+#enable ssh
+#change port in /etc/sshd_config
+sudo apt install git
+sudo apt install gcc
+sudo apt install 
+sudo apt install 
+sudo apt install 
+sudo apt install 
