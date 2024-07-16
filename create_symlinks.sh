7aec0b328bce7e3ffb1432e7004991383ce9b397
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-07-16 14:55:59 +0200

    added file for ~/.ssh/config and modified the create_symlink script to incorportate it

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -4,7 +4,7 @@ dotfile_dir="$HOME/dotfiles"
 
 cd $dotfile_dir || exit
 
-files="zshrc vimrc gitconfig bashrc"
+files="zshrc vimrc gitconfig bashrc ssh/config"
 
 for file in $files; do
 	ln -sf "$dotfile_dir/$file" "$HOME/.$file"
