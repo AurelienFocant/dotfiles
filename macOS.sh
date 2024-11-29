7723cf2a702f78c50629f5db1cc0b02a509aa500
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-11-29 18:39:53 -0300

    deleted redundant uses of killall Dock

--- macOS.sh
+++ macOS.sh
@@ -3,7 +3,7 @@
 # that need to go in ~ folder
 # --------------------------------------------------------------------
 
-dotfiles="$PWD"
+bash -c ./create_simlink.sh
 
 # --------------------------------------------------------------------
 # Also should write a script to make Download folder the one on iCloud
@@ -20,37 +20,39 @@ dotfiles="$PWD"
 defaults write com.apple.AppleMultitouchTrackpad "TrackpadThreeFingerDrag" -bool "true"
 
 # Finder: Show all file extensions inside the Finder
-defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true" && killall Finder
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 
 # Finder: Show path bar in the bottom of the Finder windows
-defaults write com.apple.finder "ShowPathbar" -bool "true" && killall Finder
+defaults write com.apple.finder "ShowPathbar" -bool "true"
 
 # Finder: Set the default view style for folders to list
-defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv" && killall Finder
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
 
 # Finder: Show extensions
-defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true" && killall Finder
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 
 # Desktop: Do not show hard disks
-defaults write com.apple.finder "ShowHardDrivesOnDesktop" -bool "false" && killall Finder
+defaults write com.apple.finder "ShowHardDrivesOnDesktop" -bool "false"
 
 # Dock: Scroll up on an icon to show app exposé
-defaults write com.apple.dock "scroll-to-open" -bool "true" && killall Dock
+defaults write com.apple.dock "scroll-to-open" -bool "true"
 
 # Dock: Autohide Dock
-defaults write com.apple.dock "autohide" -bool "true" && killall Dock
+defaults write com.apple.dock "autohide" -bool "true"
 
 # Dock: Autohide Delay
-defaults write com.apple.dock "autohide-delay" -float "0" && killall Dock
+defaults write com.apple.dock "autohide-delay" -float "0"
 
 # Mission Control : Don't arrange windows automatically  
-defaults write com.apple.dock "mru-spaces" -bool "false" && killall Dock
+defaults write com.apple.dock "mru-spaces" -bool "false"
 
 # Mission Control : Group Windows by application
-defaults write com.apple.dock "expose-group-apps" -bool "true" && killall Dock
+defaults write com.apple.dock "expose-group-apps" -bool "true"
 
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 
 # Specify the preferences directory for iterm2 settings
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/dotfiles/iTerm2_settings"
+
+killall Dock
