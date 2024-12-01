91bb8c4ef9998235c6da58d63791b43b233f8752
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-01 15:50:19 -0300

    better nvim-tree options

--- nvim/lua/nvimtree.lua
+++ nvim/lua/nvimtree.lua
@@ -1,25 +1,24 @@
 -- OPTIONS FOR Nvimtree --
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
 -- setup with some options
 require("nvim-tree").setup({
-  sort = {
-    sorter = "name",
-  },
-  view = {
-    width = 25,
-	number = true,
-	relativenumber = true,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort = {
+		sorter = "case_sensitive",
+		folders_first = false,
+	},
+	view = {
+		width = 25,
+		number = true,
+		relativenumber = true,
+	},
+	renderer = {
+		hidden_display = "simple",
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
