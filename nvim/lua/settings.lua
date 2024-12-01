38964705299c24e7db2a0e2d8f505183f14d6c95
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-01 11:29:20 -0300

    finished translating vimrc into settings.lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,8 +1,7 @@
--- Source vimrc
---vim.cmd('source ~/.vimrc') 
+local HOME = os.getenv("HOME")
 
--- Theme setup
 vim.opt.termguicolors = true
+-- Theme setup
 vim.g.tokyonight_style = "night"
 vim.cmd[[colorscheme tokyonight]]
 
@@ -13,10 +12,32 @@ vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#ff038f", bg = "NONE" })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#ff038f", bg = "NONE" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ff038f", bg = "NONE" })
 
+-- Tabs rules
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
 -- FOLDS
 vim.opt.foldmethod = "syntax"
 vim.opt.foldlevelstart = 0 
 vim.opt.foldnestmax = 1
 vim.opt.foldcolumn = "1"
-vim.api.nvim_set_keymap("n", "z", "za", { noremap = true })
 vim.api.nvim_set_hl(0, "Folded", {  bg = "NONE" })
+vim.api.nvim_set_keymap("n", "z", "za", { noremap = true })
+
+-- Add path and tags
+vim.opt.tags:append("misc/tags")
+vim.opt.path:append("include")
+
+-- Set numbers in Help Pages
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	command = "setlocal number relativenumber"
+})
+
+-- Open nvim-tree if no file is opened with vim cmd
+vim.api.nvim_create_autocmd("VimEnter", {
+	pattern = "*",
+	command = 'if expand("%") == "" | NvimTreeToggle'
+})
