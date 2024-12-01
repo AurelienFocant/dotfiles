60d8444ecc74a52a95c375fa499b976778328e1f
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-01 00:13:14 -0300

    including folds in settings.lua for nvim

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -7,9 +7,16 @@ vim.g.tokyonight_style = "night"
 vim.cmd[[colorscheme tokyonight]]
 
 -- Line Number Colors
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#ff038f", bg = "NONE" })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#ff038f", bg = "NONE" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ff038f", bg = "NONE" })
 
-vim.opt.relativenumber = true -- Enable relative line numbers
-vim.opt.number = true         -- Show the current line number
+-- FOLDS
+vim.opt.foldmethod = "syntax"
+vim.opt.foldlevelstart = 0 
+vim.opt.foldnestmax = 1
+vim.opt.foldcolumn = "1"
+vim.api.nvim_set_keymap("n", "z", "za", { noremap = true })
+vim.api.nvim_set_hl(0, "Folded", {  bg = "NONE" })
