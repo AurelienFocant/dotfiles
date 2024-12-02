e9b3e80d266bea451ee230bc3457c4b1a1515d3f
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-01 23:30:16 -0300

    changed shortcut to open and close folds back to space

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -24,7 +24,7 @@ vim.opt.foldlevelstart = 0
 vim.opt.foldnestmax = 1
 vim.opt.foldcolumn = "1"
 vim.api.nvim_set_hl(0, "Folded", {  bg = "NONE" })
-vim.api.nvim_set_keymap("n", "z", "za", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>", "za", { noremap = true })
 
 -- Add path and tags
 vim.opt.tags:append("misc/tags")
