bf249b5ee2b44cc956077e8d5e0c13a1ca685d52
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-01 22:07:55 -0300

    better tags generation

--- nvim/lua/plugins/gutentags.lua
+++ nvim/lua/plugins/gutentags.lua
@@ -1,1 +1,6 @@
+local HOME = os.getenv("HOME")
+
+os.execute('mkdir -p "misc"')
 vim.g.gutentags_ctags_tagfile = "misc/tags"
+
+vim.g.gutentags_exclude_project_root = {'/usr/local', '/opt/homebrew', '/home/linuxbrew/.linuxbrew', HOME .. '/dotfiles'}
