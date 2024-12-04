34143a9d7a2085c4bf8f9513c63b4efb16cf15dd
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-03 21:17:42 -0300

    changed names of directories

---
+++ nvim/lua/all_plugins/nvimtree.lua
@@ -0,0 +1,25 @@
+-- OPTIONS FOR Nvimtree --
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- setup with some options
+require("nvim-tree").setup({
+	sort = {
+		sorter = "case_sensitive",
+		folders_first = false,
+	},
+	view = {
+		width = 25,
+		number = true,
+		relativenumber = true,
+	},
+	renderer = {
+		hidden_display = "simple",
+	},
+	filters = {
+		dotfiles = true,
+		git_ignored = false,
+	},
+})
