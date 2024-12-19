b1d74923a1c3e2bfc41f71e892e19338d07ff8bb
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-19 11:05:13 +0100

    symlink creation now verbose

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -7,10 +7,10 @@ cd $dotfile_dir || exit
 files="zshrc vimrc gitconfig bashrc ssh/config"
 
 for file in $files; do
-	ln -sf "$dotfile_dir/$file" "$HOME/.$file"
+	ln -sfv "$dotfile_dir/$file" "$HOME/.$file"
 done
 
 # create symlinks for neovim config
 mkdir -p $HOME/.config
-rm -rf "$HOME/.config/nvim" && ln -svf "$dotfile_dir/nvim" "$HOME/.config"
+rm -rf "$HOME/.config/nvim" && ln -sfv "$dotfile_dir/nvim" "$HOME/.config"
 rm -rf "$HOME/.nvimrc" && ln -sfv "$dotfile_dir/nvim" "$HOME/.nvimrc"
