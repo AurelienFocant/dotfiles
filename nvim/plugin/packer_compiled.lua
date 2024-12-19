22f647d750c92653101597a580f7d541da370461
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-19 23:42:04 +0100

    new compiled lua plugin

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/afocant/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/afocant/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/afocant/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/afocant/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/afocant/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/aurelienfocant/.cache/nvim/packer_hererocks/2.1.1727870382/share/lua/5.1/?.lua;/Users/aurelienfocant/.cache/nvim/packer_hererocks/2.1.1727870382/share/lua/5.1/?/init.lua;/Users/aurelienfocant/.cache/nvim/packer_hererocks/2.1.1727870382/lib/luarocks/rocks-5.1/?.lua;/Users/aurelienfocant/.cache/nvim/packer_hererocks/2.1.1727870382/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/aurelienfocant/.cache/nvim/packer_hererocks/2.1.1727870382/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,47 +76,47 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   catppuccin = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/catppuccin",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/catppuccin",
     url = "https://github.com/catppuccin/nvim"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/nvim-tree/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
   ["vim-gutentags"] = {
     loaded = true,
-    path = "/home/afocant/.local/share/nvim/site/pack/packer/start/vim-gutentags",
+    path = "/Users/aurelienfocant/.local/share/nvim/site/pack/packer/start/vim-gutentags",
     url = "https://github.com/ludovicchabant/vim-gutentags"
   }
 }
