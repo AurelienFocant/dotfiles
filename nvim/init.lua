8525ae357b66236d9a2a62ac5928f55fd771212e
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-12-25 11:40:05 +0100

    changed from packer to Lazy package manager

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,8 @@
-require("settings")
+require("options")
 require("commands_keymaps")
 
-require("plugins")
+-- require("plugins")
+require("lazy_manager")
 require("all_plugins/nvimtree")
 require("all_plugins/gutentags")
 require("all_plugins/treesitter")
