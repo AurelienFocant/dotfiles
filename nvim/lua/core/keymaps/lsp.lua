0843d91e40b780cbfbe2af90e817bb183407fad0
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2025-01-23 12:39:06 +0100

    new keymaps for toggling diagnostics

--- nvim/lua/core/keymaps/lsp.lua
+++ nvim/lua/core/keymaps/lsp.lua
@@ -1,8 +1,19 @@
 -- Toggle diagnostics
+vim.keymap.set("n", "<leader>dw", function()
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end)
+
+local virt_txt = false
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*",
+	command = "lua vim.diagnostic.config({ virtual_text = false, underline = false })"
+})
 vim.keymap.set("n", "<leader>de", function()
-	if vim.diagnostic.is_enabled(nil) then
-		vim.diagnostic.enable(false)
+	if (virt_txt) then
+		virt_txt = false
+		vim.diagnostic.config({ virtual_text = false, underline = false })
 	else
-		vim.diagnostic.enable(true)
+		virt_txt = true
+		vim.diagnostic.config({ virtual_text = true, underline = true })
 	end
 end)
